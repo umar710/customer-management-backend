d020973f614d92cefc2d20dceb090ebdcd85d8a2
umar710 <umarsyed082@gmail.com>
2025-09-01 21:56:24 +0530

    Fix: updated CORS configuration in app.js

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const addressRoutes = require("./routes/addresses");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // development ke liye
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+//app.use(cors());
 app.use(express.json());
 
 // Routes
